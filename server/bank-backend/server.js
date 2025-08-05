2c9988bc0ddc2f4c21a70d3d9d1a828b83fb6969
GiriBabu-2004 <sujaykumargiri550@gmail.com>
2025-08-05 21:46:39 +0530

    fix: Remove default port fallback in server configuration

--- server/bank-backend/server.js
+++ server/bank-backend/server.js
@@ -20,5 +20,5 @@ app.get("/test", (req, res) => {
   res.send("Welcome to the SKG Bank API");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
